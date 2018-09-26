7c25b88e2d2921f925e67055a07a51cea70e1d59
Zhang <rayzhange@hotmail.com>
2018-09-26 18:33:24 -0400

    Add dependencies: Environment and Physics

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import * as AFRAME from 'aframe';
+import * as PHYSICS from 'aframe-physics-system';
+import * as ENVIRONMENT from 'aframe-environment-component';
 
 console.log(AFRAME);
+console.log(PHYSICS);
+console.log(ENVIRONMENT);
 
-console.log("Goodbye world!");
+console.log("Hello world!");

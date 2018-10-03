f0906e75607b33aeb0f0957e95330a9f48b0fe2b
Zhang <rayzhange@hotmail.com>
2018-10-03 15:57:11 -0400

    Placing objects via raycaster completed.

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ AFRAME.registerComponent('gripdown-listener', {
         this.el.addEventListener('gripdown', (event) => {
             el.setAttribute('gripdown-listener', 'gripping', 'true');
             
+            // Retrieve all intersected Elements through raycaster.
             const intersectedEls = el.components.raycaster.intersectedEls;
     
             // Check if there is intersected object.
@@ -31,12 +32,15 @@ AFRAME.registerComponent('gripdown-listener', {
                 return;
             }
     
-            this.oldPosition = el.object3D.position;
+            // Set current position as lastPosition.
+            this.lastPosition = new THREE.Vector3();
+            this.lastPosition = el.object3D.position.clone();
 
             // Set the intersected object as the following object.
             const followingEl = intersectedEls[0];
             el.setAttribute('gripdown-listener', 'followingEl', followingEl);
-            console.log('When gripping, the first intersected object is: ' + followingEl.id);
+
+            // console.log('When gripping, the first intersected object is: ' + followingEl.id);
         });
 
         this.el.addEventListener('gripup', (event) => {
@@ -44,41 +48,26 @@ AFRAME.registerComponent('gripdown-listener', {
         });
     },
 
-    tick: function(time, timeDelta) {
+    tick: function(time, timeDelta): void {
         const { gripping, followingEl } = this.data;
-        // const gripping = this.data.gripping;
-        // const followingEl = this.data.followingEl;
+
         if (gripping && followingEl) {
-            const oldPosition: any = this.oldPosition;
+            const lastPosition: any = this.lastPosition;
             const currentPosition: any = this.el.object3D.position;
-            this.oldPosition = currentPosition;
 
-            const currentTargetPosition = this.data.followingEl.getAttribute('position');
-            const updatedPosition: any = currentTargetPosition.add(currentPosition.sub(oldPosition));
+            // Store this frame's position in oldPosition.
+            this.lastPosition = currentPosition.clone();
 
-            followingEl.setAttribute('position', {x: updatedPosition.x, y: updatedPosition.y, z: updatedPosition.z});
-            // console.log('followingEl is: ' + this.data.followingEl.id);
-            console.log(typeof(oldPosition));
-            console.log('Controller current position is: ' + currentPosition.x + ',' + currentPosition.y + ','+ currentPosition.z);
-            console.log('followingEl updated position is: ' + updatedPosition.x + ',' + updatedPosition.y + ','+ updatedPosition.z);
+            // Calculate target object's position.
+            const currentTargetPosition = followingEl.getAttribute('position');
+            const updatedTargetPosition: any = currentTargetPosition.add(currentPosition.sub(lastPosition));
+
+            // Modify position at three.js level for better performance. (Better than setAttribute)
+            followingEl.object3D.position.set(updatedTargetPosition.x, updatedTargetPosition.y, updatedTargetPosition.z);
+
+            // console.log('followingEl updated position is: ' + updatedTargetPosition.x + ',' + updatedTargetPosition.y + ','+ updatedTargetPosition.z);
         }
     }
-
-    // gripdownListener: function(event) {
-    //     this.data.gripping = true;
-
-    //     var intersectedEls = this.el.components.raycaster.intersectedEls;
-
-    //     // Check if there is intersected object.
-    //     if (!Array.isArray(intersectedEls) || !intersectedEls.length) {
-    //         console.log('Nothing is intersected when gripping');
-    //         return;
-    //     }
-
-    //     // Set the intersected object as the following object.
-    //     this.data.followingEl = intersectedEls[0];
-    //     console.log('When gripping, the first intersected object is: ' + this.data.followingEl.id);
-    // }
 });
 
 AFRAME.registerComponent('intersected-listener', {
@@ -100,44 +89,3 @@ AFRAME.registerComponent('intersection-listener', {
         })
     }
 });
-
-// AFRAME.registerComponent('follow', {
-//     schema: {
-//         target: {type: 'selector'},
-//         speed: {type: 'number'}
-//     },
-
-//     init: function() {
-//         this.directionVec3 = new THREE.Vector3();
-//     },
-    
-//     tick: function(time, timeDelta) {
-//         var directionVec3 = this.directionVec3;
-
-//         // Grab position vectors (THREE.Vector3) from the entities' three.js objects.
-//         var targetPosition = this.data.target.object3D.position;
-//         var currentPosition = this.el.object3D.position;
-
-//         // Subtract the vectors to get the direction the entity should head in.
-//         directionVec3.copy(targetPosition).sub(currentPosition);
-
-//         // Calculate the distance.
-//         var distance = directionVec3.length();
-
-//         // Don't go any closer if a close proximity has been reached.
-//         if (distance < 1) { return; }
-
-//         // Scale the direction vector's magnitude down to match the speed.
-//         var factor = this.data.speed / distance;
-//         ['x', 'y', 'z'].forEach(function(axis) {
-//             directionVec3[axis] *= factor * (timeDelta / 1000);
-//         });
-
-//         // Translate the entity in the direction towards the target.
-//         this.el.setAttribute('position', {
-//             x: currentPosition.x + directionVec3.x,
-//             y: currentPosition.y + directionVec3.y,
-//             z: currentPosition.z + directionVec3.z
-//         });
-//     }
-// });

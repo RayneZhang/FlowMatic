a0cae5803d3e555dab57d67fee425de19f97bda0
Zhang <rayzhange@hotmail.com>
2018-10-28 22:20:38 -0400

    Color Wheel loaded.

--- src/components/ui/menu.ts
+++ src/components/ui/menu.ts
@@ -1,6 +1,11 @@
+declare const THREE:any;
+
 class Menu {
 
+    // Assigned in constructor as menu entity. Will be referred by many functions.
     menuEl: any = undefined;
+    // The UI objects/panels.
+    objects: any = {};
 
     constructor(ControllerEl: any) {
         const sceneEl = document.querySelector('a-scene');
@@ -23,7 +28,7 @@ class Menu {
             color: '#ffffff',
             flatShading: true,
             shader: 'flat',
-            transparent: false,
+            transparent: true,
             fog: false,
             src: '#uinormal'
         }); 
@@ -35,15 +40,9 @@ class Menu {
         // Set the visibility of the menu entity as false at the beginning.
         menuEntity.object3D.visible = false;
 
-        // Add text component to the entity.
-        // menuEntity.setAttribute('text', {
-        //     value: 'Menu',
-        //     align: 'center',
-        //     wrapCount: 24
-        // }); 
-
         // Event Listener to open and close menu.
         ControllerEl.addEventListener('xbuttondown', this.onXButtonDown.bind(this));
+        ControllerEl.addEventListener('model-loaded', this.onModelLoaded.bind(this));
 
         // Event Listener on hovering over the menu.
         // menuEntity.addEventListener('raycaster-intersected', (event) => {
@@ -69,14 +68,6 @@ class Menu {
 
     // The listener when x-button is down.
     onXButtonDown(event): void {
-        // const menuEntity: any = document.querySelector('#menu');
-        // menuEntity.object3D.visible = !menuEntity.object3D.visible;
-
-        // if (menuEntity.object3D.visible) // Add class for raycaster.
-        //     menuEntity.setAttribute('class', 'clickable');
-        // else // Remove class for raycaster.
-        //     menuEntity.removeAttribute('class');
-
         this.menuEl.object3D.visible = !this.menuEl.object3D.visible;
 
         if (this.menuEl.object3D.visible) // Add class for raycaster.
@@ -86,21 +77,92 @@ class Menu {
     }
 
     // The listener when the UI obj is loaded.
-    onModelLoaded(event): void{
-        const menuEntity: any = document.querySelector('#menu');
-        let model = menuEntity.getObject3D('mesh');
+    onModelLoaded(event): void {
+        let model = this.menuEl.getObject3D('mesh');
         model = event.detail.model;
 
         // Check the model format and whether it is empty.
         if (event.detail.format !== 'obj' || !model.getObjectByName('brightnesscursor')) {return;}
 
-        const objects: any = {};
-        objects.brightnessCursor = model.getObjectByName('brightnesscursor');
-        objects.brightnessSlider = model.getObjectByName('brightness');
-        objects.brightnessSlider.geometry.computeBoundingBox();
-        
+        this.objects.brightnessCursor = model.getObjectByName('brightnesscursor');
+        this.objects.brightnessSlider = model.getObjectByName('brightness');
+        this.objects.brightnessSlider.geometry.computeBoundingBox();
+        this.objects.previousPage = model.getObjectByName('brushprev');
+        this.objects.nextPage = model.getObjectByName('brushnext');
 
+        this.objects.hueCursor = model.getObjectByName('huecursor');
+        this.objects.hueWheel = model.getObjectByName('hue');
+        this.objects.hueWheel.geometry.computeBoundingSphere();
+        const colorWheelSize = this.objects.hueWheel.geometry.boundingSphere.radius;
+
+        this.objects.sizeCursor = model.getObjectByName('size');
+        this.objects.sizeCursor.position.copy(new THREE.Vector3(0.06409, 0.01419, -0.10242));
+
+        // this.objects.colorHistory = [];
+        // for (let i = 0; i < 7; i++) {
+        //     this.objects.colorHistory[i] = model.getObjectByName('colorhistory' + i);
+        // }
+        // this.objects.currentColor = model.getObjectByName('currentcolor');
+
+        this.objects.sizeSlider = model.getObjectByName('sizebg');
+        this.objects.sizeSlider.geometry.computeBoundingBox();
+        
+        this.initColorWheel();
+        this.updateSizeSlider();
     }
+
+    updateSizeSlider(): void {
+        var slider = this.objects.sizeSlider;
+        var sliderBoundingBox = slider.geometry.boundingBox;
+        var cursor = this.objects.sizeCursor;
+        var sliderWidth = sliderBoundingBox.max.x - sliderBoundingBox.min.x;
+        var positionX = 0.5 * sliderWidth;
+        cursor.position.setX(positionX - 0.06409);
+
+        var scale = 0.5 + 0.3;
+        cursor.scale.set(scale, 1, scale);
+    }
+
+    initColorWheel(): void {
+        var colorWheel = this.objects.hueWheel;
+    
+        var vertexShader = '\
+          varying vec2 vUv;\
+          void main() {\
+            vUv = uv;\
+            vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);\
+            gl_Position = projectionMatrix * mvPosition;\
+          }\
+          ';
+    
+        var fragmentShader = '\
+          #define M_PI2 6.28318530718\n \
+          uniform float brightness;\
+          varying vec2 vUv;\
+          vec3 hsb2rgb(in vec3 c){\
+              vec3 rgb = clamp(abs(mod(c.x * 6.0 + vec3(0.0, 4.0, 2.0), 6.0) - 3.0) - 1.0, \
+                               0.0, \
+                               1.0 );\
+              rgb = rgb * rgb * (3.0 - 2.0 * rgb);\
+              return c.z * mix( vec3(1.0), rgb, c.y);\
+          }\
+          \
+          void main() {\
+            vec2 toCenter = vec2(0.5) - vUv;\
+            float angle = atan(toCenter.y, toCenter.x);\
+            float radius = length(toCenter) * 2.0;\
+            vec3 color = hsb2rgb(vec3((angle / M_PI2) + 0.5, radius, brightness));\
+            gl_FragColor = vec4(color, 1.0);\
+          }\
+          ';
+    
+        let material = new THREE.ShaderMaterial({
+          uniforms: { brightness: { type: 'f', value: 1.0 } },
+          vertexShader: vertexShader,
+          fragmentShader: fragmentShader
+        });
+        colorWheel.material = material;
+      }
 }
 
 export default Menu;

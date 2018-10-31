a6a1b6bf9f78b002a64629c50824e93cc296178a
Zhang <rayzhange@hotmail.com>
2018-10-31 18:58:29 -0400

    Changing color, done. Minor probs with radius and shader.

--- src/components/controllers/right-trigger-listener.ts
+++ src/components/controllers/right-trigger-listener.ts
@@ -49,12 +49,67 @@ const rightTriggerListener = {
     onHueDown: function(position: any) {
         const hueWheel: any = document.querySelector('#hue');
         const hueCursor: any = document.querySelector('#huecursor');
+        const radius: number = this.calRadius(hueWheel);
 
         hueWheel.object3D.updateMatrixWorld();
         const LocalPos = hueWheel.object3D.worldToLocal(position);
 
         // console.log(LocalPos.x + ',' + LocalPos.y + ',' + LocalPos.z);       
         hueCursor.object3D.position.copy(position);
+
+        const polarPosition = {
+            r: Math.sqrt(position.x * position.x + position.z * position.z),
+            theta: Math.PI + Math.atan2(-position.z, position.x)
+        };
+        const angle: number = ((polarPosition.theta * (180 / Math.PI)) + 180) % 360;
+        const hsv = {h: angle / 360, s: polarPosition.r / radius, v: 1.0};
+        this.updateColor(hsv);
+    },
+
+    calRadius: function(hueWheel) {
+        const mesh = hueWheel.getObject3D('mesh');
+        if (!mesh) {return;}
+
+        const box = new THREE.Box3().setFromObject(mesh);
+        const size = box.getSize();
+        const extent = Math.max(size.x, size.y, size.z) / 2;
+        const radius = Math.sqrt(2) * extent;
+        
+        return radius;
+    },
+
+    updateColor: function(hsv) {
+        const rgb: any = this.hsv2rgb(hsv);
+        const color = 'rgb(' + rgb.r + ', ' + rgb.g + ', ' + rgb.b + ')';
+        
+        const box: any = document.querySelector('#box');
+        box.setAttribute('material', 'color', color);
+    },
+
+    hsv2rgb: function(hsv) {
+        var r, g, b, i, f, p, q, t;
+        var h = THREE.Math.clamp(hsv.h, 0, 1);
+        var s = THREE.Math.clamp(hsv.s, 0, 1);
+        var v = hsv.v;
+
+        i = Math.floor(h * 6);
+        f = h * 6 - i;
+        p = v * (1 - s);
+        q = v * (1 - f * s);
+        t = v * (1 - (1 - f) * s);
+        switch (i % 6) {
+        case 0: r = v; g = t; b = p; break;
+        case 1: r = q; g = v; b = p; break;
+        case 2: r = p; g = v; b = t; break;
+        case 3: r = p; g = q; b = v; break;
+        case 4: r = t; g = p; b = v; break;
+        case 5: r = v; g = p; b = q; break;
+        }
+        return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+        };
     }
 }
 

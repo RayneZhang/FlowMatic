035a8e99c1d02efc24973ae51872868ac12c0c75
Zhang <rayzhange@hotmail.com>
2018-12-05 16:57:38 -0500

    Draw curves, done. Cool.

--- src/components/lines/drawLine.ts
+++ src/components/lines/drawLine.ts
@@ -5,19 +5,21 @@ const drawLine = {
         currentLine: {type: 'selector', default: null},
         currentSource: {type: 'selector', default: null},
         startPoint: {type: 'vec3', default: {x: -1, y: 1, z: -1}},
-        endPoint: {type: 'vec3', default: {x: 1, y: 1, z: -1}}
+        endPoint: {type: 'vec3', default: {x: 1, y: 1, z: -1}},
+        divisions: {type: 'number', default: 5}
     },
 
     init: function(): void {
         
+        const positionSize = (this.data.divisions + 1) * 3;
         // Position and Color Data
-        var positions = this.positions = [];
-        var colors = this.colors = [];
+        var positions = this.positions = new Array<number>(positionSize);
+        var colors = this.colors = new Array<number>(positionSize);
 
-        positions.push(this.data.startPoint.x, this.data.startPoint.y, this.data.startPoint.z);
-        positions.push(this.data.endPoint.x, this.data.endPoint.y, this.data.endPoint.z);
-        colors.push(0, 0, 0);
-        colors.push(0, 0, 0);
+        // positions.push(this.data.startPoint.x, this.data.startPoint.y, this.data.startPoint.z);
+        // positions.push(this.data.endPoint.x, this.data.endPoint.y, this.data.endPoint.z);
+        // colors.push(0, 0, 0);
+        // colors.push(0, 0, 0);
         // console.log(line.geometry);
         // geometry.attributes.instanceEnd.data.needsUpdate = true;
     },
@@ -58,12 +60,28 @@ const drawLine = {
     },
 
     setPositions: function(): void {
-        this.positions[0] = this.data.startPoint.x;
-        this.positions[1] = this.data.startPoint.y;
-        this.positions[2] = this.data.startPoint.z;
-        this.positions[3] = this.data.endPoint.x;
-        this.positions[4] = this.data.endPoint.y;
-        this.positions[5] = this.data.endPoint.z;
+        const curvePoints = this.getCurvePoints();
+        for (let i = 0; i < this.data.divisions + 1; i++) {
+            const {x, y, z} = curvePoints[i];
+            this.positions[i * 3] = x;
+            this.positions[i * 3 + 1] = y;
+            this.positions[i * 3 + 2] = z;
+        }
+    },
+
+    getCurvePoints: function(): Array<number> {
+        const startPoint = new THREE.Vector3(this.data.startPoint.x, this.data.startPoint.y, this.data.startPoint.z);
+        const endPoint = new THREE.Vector3(this.data.endPoint.x, this.data.endPoint.y, this.data.endPoint.z);
+        const height: number = 0.5;
+
+        const controlPoint = new THREE.Vector3((startPoint.x + endPoint.x)/2, Math.max(startPoint.y, endPoint.y) + height, (startPoint.z + endPoint.z)/2);
+        const curve = new THREE.CatmullRomCurve3([
+            startPoint,
+            controlPoint,
+            endPoint
+        ]);
+
+        return curve.getPoints(this.data.divisions);
     }
 }
 

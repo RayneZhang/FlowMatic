b253283af624711c284b1c1d621456b94aeb8123
Zhang <rayzhange@hotmail.com>
2019-02-06 18:10:04 -0500

    Changed tooltips layout for user tryout.

--- src/components/controllers/tooltip-listener.ts
+++ src/components/controllers/tooltip-listener.ts
@@ -1,16 +1,18 @@
 const tooltipListener = {
     init: function(): void {
+        this.visible = true;
         const leftTooltips: any = document.querySelector("#leftTooltips");
         const rightTooltips: any = document.querySelector("#rightTooltips");
-        this.el.addEventListener('surfacetouchstart', (event) => {
-            leftTooltips.setAttribute('visible', true);
-            rightTooltips.setAttribute('visible', true);
+        this.el.addEventListener('bbuttondown', (event) => {
+            leftTooltips.setAttribute('visible', !this.visible);
+            rightTooltips.setAttribute('visible', !this.visible);
+            this.visible = !this.visible;
         });
 
-        this.el.addEventListener('surfacetouchend', (event) => {
-            leftTooltips.setAttribute('visible', false);
-            rightTooltips.setAttribute('visible', false);
-        });        
+        // this.el.addEventListener('surfacetouchend', (event) => {
+        //     leftTooltips.setAttribute('visible', false);
+        //     rightTooltips.setAttribute('visible', false);
+        // });        
     },
 
     tick: function(time, timeDelta): void {

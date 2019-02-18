652efffe7cd035c9d5c574f003a0a45c73813768
Zhang <rayzhange@hotmail.com>
2019-02-17 19:52:38 -0500

    Copy objects to component.

--- src/state-binding/index.ts
+++ src/state-binding/index.ts
@@ -6,13 +6,15 @@ import store from '../store'
 
 const stateBinding = AFRAME.registerComponent('state-binding', {
     schema: {
-        
+        objects: {type: 'array', default: []}
     },
 
     init: function(): void {
         // Log the initial state
         console.log('Initial State:');
         console.log(store.getState());
+        this.data.objects = store.getState().objects.present;
+        console.log(this.data.objects);
         // Note that subscribe() returns a function for unregistering the listener.
         const unsubscribe = store.subscribe(() => {
             this.updateObjects();

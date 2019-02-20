931dfa5bb517380eac6abd1224ff2f9582607ce0
Zhang <rayzhange@hotmail.com>
2019-02-19 22:29:04 -0500

    Two ways of implementing lookAt.

--- src/components/Utils/vector.ts
+++ src/components/Utils/vector.ts
@@ -25,12 +25,37 @@ const vector = AFRAME.registerComponent('vector', {
         subEntityTail.setAttribute('id', 'vector-tail' + this.data.seqId);
 
         this.setVectorBody(subEntityHead, subEntityTail);
-        
+
         this.setAxis();
+        this.pointAt(subEntityBody, new THREE.Vector3(1, 1, 1));
     },
     
-    pointAt: function(_position): void {
+    pointAt: function(_subEntityBody, _position): void {
+        // let xRad = 0;
+        // let yRad = 0;
+        // const startingDir = new THREE.Vector3(0, 1, 0);
 
+        // const xPlaneProject = new THREE.Vector3(0, _position.y, _position.z);
+        // xRad = startingDir.angleTo(xPlaneProject);
+        // xRad = _position.z > 0 ? xRad : -xRad;
+
+        // // Apply this x radian to starting direction.
+        // startingDir.applyAxisAngle(new THREE.Vector3(1, 0, 0), xRad);
+        // _subEntityBody.object3D.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), xRad);
+        
+        // yRad = startingDir.angleTo(_position);
+        // yRad = _position.x > 0 ? yRad : -yRad;
+
+        // // The order of rotation matters!
+        // _subEntityBody.object3D.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), yRad);
+
+        // Get the world position of current entity.
+        const worldPos = new THREE.Vector3();
+        worldPos.setFromMatrixPosition(this.el.object3D.matrixWorld);
+        const relativePosition = new THREE.Vector3(worldPos.x + _position.x, worldPos.y + _position.y, worldPos.z + _position.z);
+        // lookAt function takes a vector in world space.
+        _subEntityBody.object3D.lookAt(relativePosition);
+        _subEntityBody.object3D.rotateX(THREE.Math.degToRad(90));
     },
 
     setVectorBody: function(_subEntityHead, _subEntityTail): void {

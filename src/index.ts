5501c35309c9e5b9b968be9f0f971cb0978d025a
Zhang <rayzhange@hotmail.com>
2019-03-18 14:00:51 -0400

    Changed controller movements.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as AFRAME from 'aframe';
 import * as ENVIRONMENT from 'aframe-environment-component';
 import * as TELEPORT from 'aframe-teleport-controls';
 import * as FPSCOUNTER from 'aframe-fps-counter-component';
+import * as EXTRAS from 'aframe-extras';
+EXTRAS;
 
 import rightGripListener from './components/controllers/right-grip-listener';
 import leftTriggerListener from './components/controllers/left-trigger-listener';

6b78bfc39b2a5d1cef971d0373bb5b27b0935368
Zhang <rayzhange@hotmail.com>
2019-03-17 22:24:19 -0400

    Added aframe-effect but not displaying godrays.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as AFRAME from 'aframe';
 import * as ENVIRONMENT from 'aframe-environment-component';
 import * as TELEPORT from 'aframe-teleport-controls';
-import * as FPSCOUNTER from 'aframe-fps-counter-component'
+import * as FPSCOUNTER from 'aframe-fps-counter-component';
+import * as EFFECT from 'aframe-effects';
+EFFECT;
 
 import rightGripListener from './components/controllers/right-grip-listener';
 import leftTriggerListener from './components/controllers/left-trigger-listener';

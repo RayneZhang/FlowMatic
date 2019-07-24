32491268f952059c02a29a8d6f806c814a4cb254
Rayne <rayzhange@hotmail.com>
2019-07-23 22:55:07 -0400

    Line position updates normally. Only arrow left for line refactoring.

--- src/components/lines/lineComponent.ts
+++ src/components/lines/lineComponent.ts
@@ -33,10 +33,8 @@ const lineComponent = AFRAME.registerComponent('line-component', {
             const deltaSource = this.updatedSourcePosition.clone().sub(this.sourcePosition);
             const deltaTarget = this.updatedTargetPosition.clone().sub(this.targetPosition);
 
-            // const SP = {x: this.data.startPoint.x + deltaSource.x, y: this.data.startPoint.y + deltaSource.y, z: this.data.startPoint.z + deltaSource.z};
-            // const EP = {x: this.data.endPoint.x + deltaTarget.x, y: this.data.endPoint.y + deltaTarget.y, z: this.data.endPoint.z + deltaTarget.z};
-            const SP = {x: this.data.startPoint.x, y: this.data.startPoint.y, z: this.data.startPoint.z};
-            const EP = {x: this.data.endPoint.x, y: this.data.endPoint.y, z: this.data.endPoint.z};
+            const SP = {x: this.data.startPoint.x + deltaSource.x, y: this.data.startPoint.y + deltaSource.y, z: this.data.startPoint.z + deltaSource.z};
+            const EP = {x: this.data.endPoint.x + deltaTarget.x, y: this.data.endPoint.y + deltaTarget.y, z: this.data.endPoint.z + deltaTarget.z};
             this.el.setAttribute('line-component', 'startPoint', SP);
             this.el.setAttribute('line-component', 'endPoint', EP);
 
@@ -48,6 +46,12 @@ const lineComponent = AFRAME.registerComponent('line-component', {
     },
 
     update: function (oldDate): void {
+        if (this.data.sourceEntity) {
+            this.data.sourceEntity.object3D.getWorldPosition(this.sourcePosition);
+        }
+        if (this.data.targetEntity) {
+            this.data.targetEntity.object3D.getWorldPosition(this.targetPosition);
+        }
         if (!this.el.hasChildNodes()) {
             this.setPositions();
 

a23ed28522866b5f08fc2356a8a1af38f484ecb3
RayneZ <rayzhange@hotmail.com>
2019-08-22 05:27:30 -0400

    Create done.

--- src/components/frp/OpNodeUpdate.ts
+++ src/components/frp/OpNodeUpdate.ts
@@ -1,5 +1,7 @@
 import * as AFRAME from 'aframe'
 import { scene, Node, ObjNode, OpNode } from 'frp-backend'
+import { objects, CREATE } from '../../Objects';
+import { resize } from '../../utils/SizeConstraints';
 
 export const opNodeUpdate = AFRAME.registerComponent('op-node-update', {
     schema: {
@@ -10,12 +12,32 @@ export const opNodeUpdate = AFRAME.registerComponent('op-node-update', {
         const opNode: OpNode = scene.addOp(this.data.name);
         this.el.setAttribute('id', opNode.getID());
         
-        opNode.pluckInputs().subscribe((x) => {
-            console.log(x);
-        });
+        if (this.data.name === CREATE) {
+            opNode.pluckInputs().subscribe((input) => {
+                const object: string = input[0];
+                const pos: any = input[1];
+                create(object, pos);
+            });
+        }
+            
     },
 
     tick: function(time, timeDelta): void {
         
     }
 });
+
+function create(object: string, pos: any): void {
+    const el: any = document.createElement('a-entity');
+    const parentEl: any = document.querySelector('#redux');
+    parentEl.appendChild(el);
+
+    // Set up geometry and material
+    el.setAttribute('geometry', 'primitive', object);
+
+    // Set up position, rotation, and scale
+    el.object3D.position.copy(pos);
+    el.addEventListener('loaded', () => {
+        resize(el, 0.05);
+    });
+}

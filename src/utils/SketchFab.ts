6125ce23a6fc73c119de22b2a3dde83457d360d7
RayneZ <rayzhange@hotmail.com>
2020-01-03 19:24:24 -0500

    Token v.s. Bearer done.

--- src/utils/SketchFab.ts
+++ src/utils/SketchFab.ts
@@ -10,22 +10,13 @@ class SketchFab {
 
     constructor() {
         this.url = 'https://api.sketchfab.com/v3/search';
-
-        // const param = {
-        //     grant_type: 'password',
-        //     username: 'raynez',
-        //     password: 'Aut3mFPyVFMQQ9A'
-        // };
-        // $.post('https://sketchfab.com/oauth2/token/', param, function (data, status, xhr) {
-        //     console.log(status);
-        // });
     };
 
     getGLTFUrl(uid: string): string {
         let options: RequestInit = {
             method: 'GET',
             headers: {
-                Authorization: 'Bearer 7378591d3b564fb796e4d0976749e59e',
+                Authorization: `Token 7378591d3b564fb796e4d0976749e59e`,
             },
             mode: 'cors'
         };
@@ -33,8 +24,8 @@ class SketchFab {
         fetch('https://api.sketchfab.com/v3/models/' + uid + '/download', options).then(function(response){
             return response.json();
         }).then(function(data){
-            console.log("GLTF URL ready:");
             console.log(data);
+            const downloadURL: string = data.gltf.url;
         });
 
         return '';

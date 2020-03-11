4c916a4f40b80b8779d60869fa26aa3c19b7e354
RayneZ <rayzhange@hotmail.com>
2020-03-10 23:08:20 -0400

    Working on logic of abstracted function.

--- src/components/frp/operators/container.ts
+++ src/components/frp/operators/container.ts
@@ -23,8 +23,7 @@ export const opContainer = AFRAME.registerComponent('op-container', {
 });
 
 export function updateInOut(el: any, container: any): void {
-    // To traverse the ports of the entity, we can fetch the operator model from the op.
-    const inNames = el.getAttribute('operator-model') ? el.getAttribute('operator-model').functionInputs : [];
+    const inNames: Array<string> = el.getAttribute('operator-model') ? el.getAttribute('operator-model').functionInputs : [];
     const outNames = el.getAttribute('operator-model') ? el.getAttribute('operator-model').functionOutputs : [];
     const inBvrs = el.getAttribute('operator-model') ? el.getAttribute('operator-model').behaviorInputs : [];
     const outBvrs = el.getAttribute('operator-model') ? el.getAttribute('operator-model').behaviorOutputs : [];
@@ -35,26 +34,61 @@ export function updateInOut(el: any, container: any): void {
     const inPorts = container.getAttribute('op-container') ? container.getAttribute('op-container').inPorts: [];
     const outPorts = container.getAttribute('op-container') ? container.getAttribute('op-container').outPorts: [];
 
+    // For each input port of the operator, add it to the container temporarily.
+    inNames.forEach((inName: string, i: number) => {
+        const p: Port = {name: inName, type: inTypes[i], behavior: inBvrs[i]};
+        inPorts.push(p);
+    });
+
+    // For each output port of the operator, add it to the container temporarily.
+    outNames.forEach((outName: string, i: number) => {
+        const p: Port = {name: outName, type: outTypes[i], behavior: outBvrs[i]};
+        outPorts.push(p);
+    });
+
     const incomingEdges = el.getAttribute('stored-edges') ? el.getAttribute('stored-edges').incomingEdges : [];
-
     incomingEdges.forEach((edgeID: string) => {
-        const edge: any = document.querySelector('#' + edgeID);
-        if (edge) {
-            // If all the edges are coming from within the container, then omit this port.
-            // For each edge we can retrieve the line-component from the edge, but we also need to tranverse the ports of the entity
+        const edgeEl: any = document.getElementById(edgeID);
+        if (edgeEl) {
+            // For each edge we can retrieve the line-component from the edge.
+            const srcEl: any = edgeEl.getAttribute('line-component').sourceEntity;
+            const srcId: string = srcEl.getAttribute('id');
 
-            // Next we retrieve the ops that exsit in the container already.
-            
+            // If the src entity id is in the opList of the container, then we should omit both ports
+            if (opList.indexOf(srcId) != -1) {
+                const srcProp: string = edgeEl.getAttribute('line-component').sourceProp;
+                const tgtProp: string = edgeEl.getAttribute('line-component').targetProp;
 
+                // 1. Omit the inPort of the target entity.
+                const idx: number = inNames.indexOf(tgtProp);
+                inPorts.splice(idx, 1);
+
+                // 2. Omit the outPort of the source entity (which might have been omitted).
+
+            }
         }
     });
 
     const outgoingEdges = el.getAttribute('stored-edges') ? el.getAttribute('stored-edges').outgoingEdges : [];
     outgoingEdges.forEach((edgeID: string) => {
-        const edge: any = document.querySelector('#' + edgeID);
-        if (edge) {
-            // If all the edges are going to within the container, then omit this port.
-            console.log(edge);
+        const edgeEl: any = document.getElementById(edgeID);
+        if (edgeEl) {
+            // For each edge we can retrieve the line-component from the edge.
+            const tgtEl: any = edgeEl.getAttribute('line-component').targetEntity;
+            const tgtId: string = tgtEl.getAttribute('id');
+
+            // If the tgt entity id is in the opList of the container, then we should omit both ports
+            if (opList.indexOf(tgtId) != -1) {
+                const srcProp: string = edgeEl.getAttribute('line-component').sourceProp;
+                const tgtProp: string = edgeEl.getAttribute('line-component').targetProp;
+
+                // 1. Omit the outPort of the source entity.
+                const idx: number = outNames.indexOf(srcProp);
+                outPorts.splice(idx, 1);
+
+                // 2. Omit the inPort of the target entity (which might have been omitted).
+
+            }
         }
     });
 
